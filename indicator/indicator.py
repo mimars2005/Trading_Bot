605a86e6ea9488a23a30c2d318f124674e1cf4e0
mimars2005 <mi6kata.com@gmail.com>
2024-12-25 11:11:10 +0100

    indicators structure created

---
+++ indicator/indicator.py
@@ -0,0 +1,19 @@
+from abc import ABC,abstractmethod
+
+class Indicator(ABC):
+    def getData():
+        pass
+
+class BB(Indicator): # Bollinger Band
+    def getData():
+        return
+    
+    
+class MA(Indicator): # Moving Average
+    def getData():
+        return
+    
+    
+class Stochastic(Indicator): # Stochastic indicator
+    def getData():
+        return
