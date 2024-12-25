9659682b37b28a0290596c54b33b01879af7c3a8
mimars2005 <mi6kata.com@gmail.com>
2024-12-25 11:10:16 +0100

    modified classes in one file

---
+++ strategy/strategy.py
@@ -0,0 +1,13 @@
+from abc import ABC,abstractmethod
+
+class Strategy(ABC):
+    def make_decision():
+        pass
+
+class BBRS(Strategy):
+    def make_decision():
+        return
+        
+class MovingAverage(Strategy):
+    def make_decision():
+        return
